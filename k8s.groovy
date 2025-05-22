4d1a9d281105c964e630c981907e7d24a9b1ccee
vikasghandge <ghandgevikas804@gmail.com>
2025-05-22 20:19:25 +0530

    --- Auto Git Commit ---

--- k8s.groovy
+++ k8s.groovy
@@ -56,7 +56,7 @@ pipeline {
          stage('OWASP FS SCAN') {
             steps {
               dependencyCheck additionalArguments: '--scan ./ --disableYarnAudit --disableNodeAudit', odcInstallation: 'DP-Check'
-      //          dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+                 dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
      //       }
     //    }
       //  */
